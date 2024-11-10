1565e4e9e0ffb0da38d8dfc626f6c426e2eba43e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-10 09:50:40 +0100

    Update home.py

--- home.py
+++ home.py
@@ -9,9 +9,9 @@ df_references = conn.read(ttl=0,worksheet="multipage_users")
 
 option = st.selectbox("Who are you?",("Luigi", "Daan", "Tommaso"),)
 
-page_1 = st.Page("page_1.py", title="page 1", icon=":material/search:")
-page_2 = st.Page("page_2.py", title="page 2", icon=":material/search:")
-page_3 = st.Page("page_3.py", title="page 3", icon=":material/search:")
+page_1 = st.Page("page_1.py", title="page 1", )
+page_2 = st.Page("page_2.py", title="page 2")
+page_3 = st.Page("page_3.py", title="page 3")
 
 status = df_references[df_references['name']==option].reset_index(drop=True)['type'].loc[0]
 # st.write(status)
