589d1dc8394500153f48434031fd3c36a4bf20fb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-10 09:55:54 +0100

    Update home.py

--- home.py
+++ home.py
@@ -11,8 +11,8 @@ status = df_references[df_references['name']==option].reset_index(drop=True)['ty
 
 
 page_1 = st.Page("page_1.py", title="page 1", )
-page_2 = st.Page("page_2.py", title="page 2")
-page_3 = st.Page("page_3.py", title="page 3")
+page_2 = st.Page("page/page_2.py", title="page 2")
+page_3 = st.Page("page/page_3.py", title="page 3")
 
 if status == 'admin':
     pg = st.navigation(
