57e30df558420137a3be7d4bb91b0ac28920f306
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-10 09:35:47 +0100

    Update home.py

--- home.py
+++ home.py
@@ -1,41 +1,28 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
 
-def login():
-    if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+conn = st.connection("gsheets", type=GSheetsConnection)
+df_references = conn.read(ttl=0,worksheet="multipage_users")
 
-def logout():
-    if st.button("Log out"):
-        st.session_state.logged_in = False
-        st.rerun()
+st.dataframe(df_references)
 
-login_page = st.Page(login, title="Log in", icon=":material/login:")
-logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 
-dashboard = st.Page(
-    "reports/dashboard.py", title="Dashboard", icon=":material/dashboard:", default=True
-)
-bugs = st.Page("reports/bugs.py", title="Bug reports", icon=":material/bug_report:")
-alerts = st.Page(
-    "reports/alerts.py", title="System alerts", icon=":material/notification_important:"
-)
+# search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+# search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+# search = st.Page("tools/search.py", title="Search", icon=":material/search:")
+# search = st.Page("tools/search.py", title="Search", icon=":material/search:")
 
-search = st.Page("tools/search.py", title="Search", icon=":material/search:")
-history = st.Page("tools/history.py", title="History", icon=":material/history:")
 
-if st.session_state.logged_in:
-    pg = st.navigation(
-        {
-            "Account": [logout_page],
-            "Reports": [dashboard, bugs, alerts],
-            "Tools": [search, history],
-        }
-    )
-else:
-    pg = st.navigation([login_page])
+# if st.session_state.logged_in:
+#     pg = st.navigation(
+#         {
+#             "Account": [logout_page],
+#             "Reports": [dashboard, bugs, alerts],
+#             "Tools": [search, history],
+#         }
+#     )
+# else:
+#     pg = st.navigation([login_page])
 
-pg.run()
+# pg.run()
