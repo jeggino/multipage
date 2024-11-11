3e6590165b2ed03acb2701dfd861265158fb9d7f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-11 10:27:08 +0100

    Update page_3.py

--- page/page_3.py
+++ page/page_3.py
@@ -1,43 +1,109 @@
 import streamlit as st
+from streamlit_gsheets import GSheetsConnection
 import pandas as pd
-import numpy as np
-import pydeck as pdk
+import datetime
+from datetime import datetime, timedelta, date
 
-st.write(f'WLKON {st.session_state.login["name"]}')
+from credentials import *
 
-chart_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=["lat", "lon"],
+
+
+# ---LAYOUT---
+st.set_page_config(
+    page_title="🦇🪶 SMPs",
+    initial_sidebar_state="collapsed",
+    page_icon="🦇🪶",
+    layout="centered",
 )
 
-chart_data["capital-cities"] = 'Roma'
 
-chart_data
+st.markdown("""
+    <style>
+    .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
+    </style>
+    """,
+    unsafe_allow_html=True)
 
-chart=pdk.Deck(
-        map_style=None,
-        initial_view_state=pdk.ViewState(
-            latitude=37.76,
-            longitude=-122.4,
-            zoom=11,
-            pitch=50,
-        ),
-    tooltip={"text": "{capital-cities}"},
-        layers=[
-            pdk.Layer(
-                "ScatterplotLayer",
-                data=chart_data,
-                pickable=True,
-                get_position="[lon, lat]",
-                get_color="[200, 30, 0, 160]",
-                get_radius=100,
-                id="capital-cities",
-            ),
-        ],
-    )
 
-event = st.pydeck_chart(chart, on_select="rerun", selection_mode="single-object")
 
-event.selection
+reduce_header_height_style = """
+<style>
+    div.block-container {padding-top: 1rem; padding-bottom: 0rem; padding-left: 1rem; padding-right: 1rem; margin-top: 1rem; margin-bottom: 0rem;}
+</style>
+""" 
 
-st.sidebar.write('Map')
+st.markdown(reduce_header_height_style, unsafe_allow_html=True)
+
+# --- FUNCTIONS ---
+def insert_dagverslag(waarnemer,project,opdracht,gebied_id,doel,datum,start_time,eind_time,extra_velfwerker,
+                      temperatuur,bewolking,neerslag,windkrcht,windrichting,opmerking,df_old):
+    
+    data = [{"waarnemer":waarnemer,"project":project,"opdracht":opdracht,"gebied_id":gebied_id,'doel':doel,"datum":datum,
+             "start_time":start_time,"eind_time":eind_time, "extra_velfwerker":extra_velfwerker, "temperatuur":temperatuur, "bewolking":bewolking,
+             "neerslag":neerslag,"windkrcht":windkrcht,"windrichting":windrichting,"opmerking":opmerking}]
+    df_new = pd.DataFrame(data)
+    df_updated = pd.concat([df_old,df_new],ignore_index=True)
+    
+    return conn.update(worksheet="df_ekomaps_dagverslagen",data=df_updated)
+
+#---DATASET---
+ttl = '10m'
+ttl_references = '10m'
+conn = st.connection("gsheets", type=GSheetsConnection)
+df_old = conn.read(ttl=ttl,worksheet="df_ekomaps_dagverslagen")
+df_projects = conn.read(ttl=ttl_references,worksheet="df_ekomaps_projects")
+
+# --- APP ---
+IMAGE = "image/logo.png"
+IMAGE_2 ="image/menu.jpg"
+st.logo(IMAGE,  link=None, icon_image=IMAGE_2)
+
+try:
+    waarnemer = st.session_state.login['name']
+    project = st.session_state.project['project_name']
+    opdracht = st.session_state.project['opdracht']
+    gebied_id = st.session_state.project['area']
+    
+    st.title(f'{project}')
+    st.header(f'Opdracht: **{opdracht}**',divider=True)
+    
+    if gebied_id == None:
+        pass
+    else:
+        text = f'Gebied: **{gebied_id }**'
+        st.subheader(text,divider=True)
+    
+    with st.form("my_form", clear_on_submit=True,border=False):
+        
+        if opdracht == 'Vleermuizen':
+            doel = st.selectbox('Doel',('Kraamverblijf','Winterverblijf','Paarverblijf'))
+        elif opdracht == 'Vogels':
+            doel = st.selectbox('Doel',BIRD_NAMES)
+            
+        datum = st.date_input("Datum","today")       
+        two_hours_from_now = datetime.now() + timedelta(hours=1)
+        four_hours_from_now = datetime.now() + timedelta(hours=3)
+        start_time = st.time_input("Start tijd", two_hours_from_now)
+        eind_time = st.time_input("Eind tijd", four_hours_from_now)
+        
+        extra_velfwerker_list = df_projects.set_index('project').loc[project,"user"].split(',')
+        extra_velfwerker_list.remove(waarnemer)
+        extra_velfwerker = st.multiselect("Extra veldwerker",extra_velfwerker_list)
+        
+        temperatuur = st.number_input("Temperatuur",key='temperatuur', min_value=0)
+        bewolking = st.selectbox("Bewolking",("Onbewolkt (<10%)", "Halfbewolkt (10-80%)", "Bewolkt (>80%)"))
+        neerslag = st.selectbox("Neerslag",("Droog", "Nevel/mist", "Motregen", "Regen","Zware regen","Sneeuw"))
+        windkrcht = st.number_input("Windkracht",key='windkrcht', min_value=1)
+        windrichting = st.selectbox("Windrichting",("Noord", "Noordoost", "Oost", "Zuidoost","Zuid","Zuidwest","West","Noordwest"))
+        
+        if gebied_id == None:
+            st.markdown("Vergeet a.u.b. niet in de opmerking te schrijven welke soort je hebt gevonden, de dichtstbijzijnde locaties en het doel van het onderzoek.")
+            
+        opmerking = st.text_input("", placeholder="Vul hier een opmerking in ...")
+        
+        if st.form_submit_button("**Gegevens opslaan**",use_container_width=True):
+            insert_dagverslag(waarnemer,project,opdracht,gebied_id,doel,datum,start_time,eind_time,extra_velfwerker,temperatuur,bewolking,neerslag,windkrcht,windrichting,opmerking,df_old)
+        
+            st.switch_page("page/🧭_navigatie.py")
+except:
+    st.switch_page("page/🧭_navigatie.py")
