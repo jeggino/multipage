7d3abad49ca9cc084eb11862ac674f9e0a80bd93
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-11 11:12:07 +0100

    Update 📊_ Statistik.py

--- page/📊_ Statistik.py
+++ page/📊_ Statistik.py
@@ -15,13 +15,13 @@ import random
 from credentials import *
 
 
-# ---LAYOUT---
-st.set_page_config(
-    page_title="🦇🪶 SMPs",
-    initial_sidebar_state="collapsed",
-    page_icon="🦇🪶",
-    layout="wide",
-)
+# # ---LAYOUT---
+# st.set_page_config(
+#     page_title="🦇🪶 SMPs",
+#     initial_sidebar_state="collapsed",
+#     page_icon="🦇🪶",
+#     layout="wide",
+# )
 
 
 st.markdown("""
