50c757dab3e012191239f030c7d46d16760a321d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-11-24 08:10:56 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -103,8 +103,8 @@ icon_dictionary = {'Vogels': {'Gierzwaluw': {'vogel waarneming': 'icons/swift.pn
                                                      'mogelijke nestlocatie': 'icons/mogelijk_nest.png'}}
                   }
 
-DICTIONARY_PROJECTS = {"Overig":["Vogels","Vleermuizen","Vleermuiskast","Vogels-Overig"],
-                      "Admin":["Vogels","Vleermuizen","Vleermuiskast","Vogels-Overig"],
+DICTIONARY_PROJECTS = {"Overig":["Vogels","Vleermuizen","Vogels-Overig"],
+                      "Admin":["Vogels","Vleermuizen","Vogels-Overig"],
                        "Bats-balkans":["Vleermuizen"],
                        "SMPs-Amsterdam (Niewe West)":["Vleermuizen"],
                        "SMPs-Amsterdam (Noord)":["Vleermuizen"],
