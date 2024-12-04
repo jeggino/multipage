1ac7f84569ef27da38c1168d1d7e51b8c5269752
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-04 19:35:01 +0100

    Update ✍️_Voeg_een_waarneming_in.py

--- page/✍️_Voeg_een_waarneming_in.py
+++ page/✍️_Voeg_een_waarneming_in.py
@@ -84,17 +84,17 @@ def map():
     LocateControl(auto_start=False,position="topright").add_to(m)
     folium.LayerControl().add_to(m)     
     
-    try:
-        folium.GeoJson(
-            st.session_state.project['gdf'],
-            name=f"*Gebied: {st.session_state.project['area']}",
-            style_function=lambda feature: {
-            "color": "black",
-            "weight": 3,
-            },
-        ).add_to(m)
-    except:
-        pass
+    # try:
+    #     folium.GeoJson(
+    #         st.session_state.project['gdf'],
+    #         name=f"*Gebied: {st.session_state.project['area']}",
+    #         style_function=lambda feature: {
+    #         "color": "black",
+    #         "weight": 3,
+    #         },
+    #     ).add_to(m)
+    # except:
+    #     pass
         
     output = st_folium(m, returned_objects=["all_drawings"],width=OUTPUT_width, height=OUTPUT_height)
     output["features"] = output.pop("all_drawings")
