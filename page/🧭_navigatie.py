3e2441804da659ba0becd48da6aa0f3ee1d13de2
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-04 20:15:08 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -451,7 +451,7 @@ folium.TileLayer(tiles='https://api.mapbox.com/styles/v1/jeggino/cm2vtvb2l000w01
 try:
     folium.GeoJson(
         st.session_state.project['gdf'],
-        name=f"*Gebied: {st.session_state.project['area']}",
+        name=f"Gebiedsgrens",
         style_function=lambda feature: {
             "color": "black",
             "weight": 3,
