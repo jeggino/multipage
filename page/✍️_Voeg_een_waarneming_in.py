f8df7ab56a67bc830836c76eb7ddc3958953ddcd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-04 19:13:03 +0100

    Update ✍️_Voeg_een_waarneming_in.py

--- page/✍️_Voeg_een_waarneming_in.py
+++ page/✍️_Voeg_een_waarneming_in.py
@@ -60,8 +60,15 @@ def insert_json(key,waarnemer,datum,time,soortgroup,aantal,sp,gedrag,functie,ver
     return conn.update(worksheet="df_observations",data=df_updated)
 
 def map():
+
+    try:
+        lat = st.session_state.project['gdf'].centroid.y.mean()
+        lng = st.session_state.project['gdf'].centroid.x.mean()
+        
+        m = folium.Map(location=[lat, lng],zoom_start=10)
+    except:
+        m = folium.Map()    
     
-    m = folium.Map()
     folium.TileLayer(tiles='https://api.mapbox.com/styles/v1/jeggino/cm2vtvb2l000w01qz9wet0mv9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamVnZ2lubyIsImEiOiJjbHdscmRkZHAxMTl1MmlyeTJpb3Z2eHdzIn0.N9TRN7xxTikk235dVs1YeQ',
                  attr='XXX Mapbox Attribution',overlay=False,show=False,name="Satellietkaart").add_to(m)
     
