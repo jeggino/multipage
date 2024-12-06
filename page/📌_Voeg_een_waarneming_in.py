633915363c63e13da4c4870589613c408f1ff9fb
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-06 13:07:26 +0100

    Update 📌_Voeg_een_waarneming_in.py

--- page/📌_Voeg_een_waarneming_in.py
+++ page/📌_Voeg_een_waarneming_in.py
@@ -190,7 +190,7 @@ def input_data(output,df_old):
 try:
     IMAGE = "image/logo.png"
     IMAGE_2 ="image/menu.jpg"
-    st.logo(IMAGE_2,  link=None, icon_image=IMAGE_2)
+    st.logo(IMAGE,  link=None, icon_image=IMAGE)
     
     waarnemer = st.session_state.login['name']
     
