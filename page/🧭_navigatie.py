d5c8e7b2ac4259448ab3a19f40188d12fa37861f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-07 12:24:28 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -401,8 +401,8 @@ except:
     map = folium.Map(tiles=None)
     
 
-LocateControl(auto_start=False,position="topleft").add_to(map)
-Fullscreen(position="topleft").add_to(map)
+# LocateControl(auto_start=False,position="topleft").add_to(map)
+# Fullscreen(position="topleft").add_to(map)
 
 # functie_dictionary = {}
 
