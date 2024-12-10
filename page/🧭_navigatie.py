3e62b20fa0d20e814aaa1026c25cbc4784c39f3c
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-10 11:14:34 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -426,7 +426,7 @@ folium.TileLayer(tiles="CartoDB Positron",overlay=False,show=False,name="Witte k
 folium.TileLayer(tiles='https://api.mapbox.com/styles/v1/jeggino/cm2vtvb2l000w01qz9wet0mv9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamVnZ2lubyIsImEiOiJjbHdscmRkZHAxMTl1MmlyeTJpb3Z2eHdzIn0.N9TRN7xxTikk235dVs1YeQ',
                  attr='XXX Mapbox Attribution',overlay=False,show=False,name="Satellietkaart").add_to(map)
 
-names = folium.FeatureGroup(name="Gebiedsnamen")
+# names = folium.FeatureGroup(name="Gebiedsnamen")
 
 try:
     folium.GeoJson(
@@ -443,9 +443,6 @@ try:
 
     folium.GeoJson(
         gdf_names,
-        tooltip=folium.GeoJsonTooltip(fields=['Gebied'],
-                                             aliases=['Gebied'],
-                                     ),
         name="Gebiedsnamen",
     ).add_to(map)
     # for row,columns in gdf_names.iterrows():
