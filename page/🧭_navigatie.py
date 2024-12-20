1f34490d4856f9efb3afb0987ba3d580711b53f6
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-20 12:08:29 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -8,6 +8,8 @@ import folium
 from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
 from folium.features import DivIcon
 from streamlit_folium import st_folium
+from branca.element import Template, MacroElement, IFrame
+
 import datetime
 from datetime import datetime, timedelta, date
 import random
@@ -67,6 +69,88 @@ ICON_SIZE_BIRD = (70,50)
 ICON_SIZE_Huiszwaluw = (80,45)
 
 # --- FUNCTIONS ---
+
+
+def legend(species_colors_dict,dragable=True):
+
+    
+    legend_temp=''
+    
+    
+    for species in species_colors_dict.keys():
+        legend_temp = legend_temp + f"<li><span style='background: {species_colors_dict[species]}; opacity: 0.75;'></span>{species}</li>"
+        
+    
+    legend_body = f"""  
+    <!doctype html>
+    <html lang="en">
+    <body>
+    <div id='maplegend' class='maplegend' 
+        style='position: absolute; z-index: 9999; background-color: rgba(255, 255, 255, 0.7);
+         border-radius: 8px; padding: 10px; font-size: 11px; right: 10px; bottom: 35px; '>     
+    <div class='legend-scale'>
+      <ul class='legend-labels'>
+          <img src="https://www.elskenecologie.nl/wp-content/themes/elsken/assets/img/logo-mobile.png" style="background:green; width:30px;height:23px;">
+          <li>------------</li>
+        <li><strong>Sorten</strong></li>
+
+        {legend_temp}
+
+        <li><strong>Functie</strong></li>
+        <li><span class="fa fa-map-marker" style="color:grey" opacity: 0.75;'></span>Waarneming</li>
+        <li><span class="fa fa-object-ungroup" style="color:grey" opacity: 0.75;'></span>Gebied</li>
+      </ul> 
+    </body>
+    </html>
+    """
+    
+    
+    legend_style = """<style type='text/css'>
+      .maplegend .legend-scale ul {margin: 0; padding: 0; color: #0f0f0f;}
+      .maplegend .legend-scale ul li {list-style: none; line-height: 18px; margin-bottom: 1.5px;}
+      .maplegend ul.legend-labels li span {float: left; height: 16px; width: 16px; margin-right: 4.5px;}
+    </style>
+    
+    {% endmacro %}
+    """
+    legend_dragable = """{% macro html(this, kwargs) %}
+        <!doctype html>
+        <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <title>jQuery UI Draggable - Default functionality</title>
+          <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        
+          <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+          <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+          
+          <script>
+          $( function() {
+            $( "#maplegend" ).draggable({
+                            start: function (event, ui) {
+                                $(this).css({
+                                    right: "auto",
+                                    top: "auto",
+                                    bottom: "auto"
+                                });
+                            }
+                        });
+        });
+        
+          </script>
+        </head>
+        """
+    
+    legend_normal = "{% macro html(this, kwargs) %}"
+
+    if dragable == True:
+        legend = legend_dragable + legend_body + legend_style
+    else:
+        legend = legend_normal + legend_body + legend_style
+
+    return legend
+    
 def popup_polygons(row):
     
     i = row
@@ -426,7 +510,6 @@ folium.TileLayer(tiles='https://api.mapbox.com/styles/v1/jeggino/cm2vtvb2l000w01
                  attr='XXX Mapbox Attribution',overlay=False,show=False,name="Satellietkaart").add_to(map)
 
 
-
 try:
        
     folium.GeoJson(
@@ -526,6 +609,10 @@ for i in range(len(df_2)):
                       ).add_to(fouctie_loop)
 
 folium.LayerControl().add_to(map)
+legend_template = legend(species_colors_dict,True)
+macro = MacroElement()
+macro._template = Template(legend_template)
+map.add_child(macro)
 
 output = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,
                      feature_group_to_add=list(functie_dictionary.values()))
