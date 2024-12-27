a4507ad7ffdc502ceb10f17ea42de826beffe369
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-27 10:52:31 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -621,28 +621,29 @@ if st.session_state.project['opdracht'] == 'Vleermuizen':
 
 output = st_folium(map,returned_objects=["last_active_drawing"],width=OUTPUT_width, height=OUTPUT_height,
                      feature_group_to_add=list(functie_dictionary.values()))
-    
-try:
+
+if st.session_state.login['type'] == 'user':
     try:
-        id = str(output["last_active_drawing"]['geometry']['coordinates'][0])+str(output["last_active_drawing"]['geometry']['coordinates'][1])
-        name = f"{id}"
+        try:
+            id = str(output["last_active_drawing"]['geometry']['coordinates'][0])+str(output["last_active_drawing"]['geometry']['coordinates'][1])
+            name = f"{id}"
+        except:
+            id = str(output["last_active_drawing"]['geometry']['coordinates'][0][0][0])+str(output["last_active_drawing"]['geometry']['coordinates'][0][0][1])
+            name = f"{id}"
+        
+        with st.sidebar:
+            if st.button("Waarneming bijwerken",use_container_width=True):
+                update_item(id)
+            if st.button(":red[**Verwijder waarneming**]",use_container_width=True):
+                df = conn.read(ttl=0,worksheet="df_observations")
+                df_filter = df_point[df_point["key"]==id]
+                df_drop = df_point[~df_point.apply(tuple, axis=1).isin(df_filter.apply(tuple, axis=1))]
+                conn.update(worksheet='df_observations',data=df_drop)
+                st.success('Waarneming verwijderd', icon="✅")
+                st.page_link('page/🧭_navigatie.py', label="Opnieuw opstarten", icon="♻️",use_container_width=True)
+                                           
     except:
-        id = str(output["last_active_drawing"]['geometry']['coordinates'][0][0][0])+str(output["last_active_drawing"]['geometry']['coordinates'][0][0][1])
-        name = f"{id}"
-    
-    with st.sidebar:
-        if st.button("Waarneming bijwerken",use_container_width=True):
-            update_item(id)
-        if st.button(":red[**Verwijder waarneming**]",use_container_width=True):
-            df = conn.read(ttl=0,worksheet="df_observations")
-            df_filter = df_point[df_point["key"]==id]
-            df_drop = df_point[~df_point.apply(tuple, axis=1).isin(df_filter.apply(tuple, axis=1))]
-            conn.update(worksheet='df_observations',data=df_drop)
-            st.success('Waarneming verwijderd', icon="✅")
-            st.page_link('page/🧭_navigatie.py', label="Opnieuw opstarten", icon="♻️",use_container_width=True)
-                                       
-except:
-    st.stop()
+        st.stop()
 
 # except:
 #     st.image("https://media.istockphoto.com/photos/open-empty-cardboard-box-on-a-white-background-picture-id172167710?k=6&m=172167710&s=612x612&w=0&h=Z4fueCweh9q-X_VBRAPCYSalyaAnXG3ioErb8oJSVek=")
