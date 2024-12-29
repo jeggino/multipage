a89208841c81266b6ce92bbc834966d7e26a4cd5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-29 13:54:14 +0100

    Update 📊_ Statistik.py

--- page/📊_ Statistik.py
+++ page/📊_ Statistik.py
@@ -32,7 +32,7 @@ st.logo(IMAGE,  link=None, size="large",icon_image=IMAGE)
 for key in media_dict[st.session_state.project['opdracht']]:
     col1,col2 = st.columns([2,1],gap="large", vertical_alignment="top", border=True)
     col1.video(key)
-    col2.write(st.session_state.project['opdracht'][key])
+    col2.write(media_dict[st.session_state.project['opdracht']][key])
     "---"
 # except:
 #     st.write('Not yet')
