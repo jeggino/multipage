e590c6827021f2994f385c158840c70c8d05202e
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-29 10:22:39 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -481,9 +481,9 @@ try:
                                    else (icon_dictionary[x["soortgroup"]][x["sp"]][x["functie"]] if x["soortgroup"] in ['Vogels','Vleermuizen',"Vogels-Overig"] 
                                          else icon_dictionary[x["soortgroup"]][x["functie"]]), 
                                    axis=1)
-    
-    df_2 = df_2.reset_index(drop=True)
     st.error('!!!!!!!!')
+    df_2 = df_2.reset_index(drop=True)
+    
 
     df_overig["icon_data"] = df_overig.apply(lambda x: None if x["geometry_type"] in ["LineString","Polygon"] 
                                    else (icon_dictionary[x["soortgroup"]][x["sp"]][x["functie"]] if x["soortgroup"] in ['Vogels','Vleermuizen',"Vogels-Overig"] 
