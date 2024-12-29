21c838eb86aeef3052aec76b1cadb69f7109e95a
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-29 13:49:57 +0100

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -1,3 +1,3 @@
 media_dict = {'Vleermuizen':{"https://www.youtube.com/watch?v=OKEaI1mFAhc":"",
                             'https://www.youtube.com/watch?v=6qO1mpw8Xow':'Laatvliegers foerageren boven fietspad te Lies, Terschelling. Lantaarnpaal trekt waarschijnlijk prooien aan en de laatvliegers maken daar gretig gebruik van.'},
-             'Vogels':{}}
+             'Vogels':{'https://www.youtube.com/shorts/uXhK7BwJz7A':'ljkghlsd'}}
