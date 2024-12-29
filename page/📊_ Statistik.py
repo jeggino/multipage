7066e470005d04142e5fec153f6ddea14384e27d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2024-12-29 13:56:43 +0100

    Update 📊_ Statistik.py

--- page/📊_ Statistik.py
+++ page/📊_ Statistik.py
@@ -28,14 +28,14 @@ st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 IMAGE = "image/logo.png"
 st.logo(IMAGE,  link=None, size="large",icon_image=IMAGE)
 
-# try:
-for key in media_dict[st.session_state.project['opdracht']]:
-    col1,col2 = st.columns([2,1],gap="large", vertical_alignment="top", border=True)
-    col1.video(key)
-    col2.write(media_dict[st.session_state.project['opdracht']][key])
-    "---"
-# except:
-#     st.write('Not yet')
-#     # st.image('https://www.flickr.com/photos/67492897@N07/52814358250/in/dateposted/')
-#     st.write('Not yet')
+try:
+    for key in media_dict[st.session_state.project['opdracht']]:
+        col1,col2 = st.columns([2,1],gap="large", vertical_alignment="top", border=True)
+        col1.video(key)
+        col2.write(media_dict[st.session_state.project['opdracht']][key])
+        "---"
+except:
+    st.write('Not yet')
+    st.image('https://flic.kr/p/2ot2oFC')
+    st.write('Not yet')
    
