6052fea844051eb7b43c4eb2e75ebc53de2618d6
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-02 11:31:26 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -20,7 +20,7 @@ from credentials import *
 
 
 #---DATASET---
-ttl = 0
+ttl = 10
 ttl_references = 30
 conn = st.connection("gsheets", type=GSheetsConnection)
 df_point = conn.read(ttl=ttl,worksheet="df_observations")
