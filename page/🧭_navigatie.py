568a3ee472b29d64c2c5d7d9330bb0bb37e651fa
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-02 10:54:54 +0100

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -101,8 +101,8 @@ def legend(species_colors_dict,dragable=True):
         <li><span class="fa fa-snowflake" style="color:grey" opacity: 0.75;'></span>Winterverblijf</li>
         <li><span class="fa fa-heart" style="color:grey" opacity: 0.75;'></span>Paarverblijf</li>
         <li><strong>Functiegebied</strong></li>
-        <li><span class="fa-regular fa-clone" style="color:grey" opacity: 0.75;'></span>Foerageergebied</li>
-        <li><span class="fa-solid fa-clone" style="color:grey" opacity: 0.75;'></span>Baltsterritorium</li>
+        <li><span class="fa-solid fa-clone" style="color:grey" opacity: 0.75;'></span>Foerageergebied</li>
+        <li><span class="fa-regular fa-clone" style="color:grey" opacity: 0.75;'></span>Baltsterritorium</li>
       </ul> 
     </body>
     </html>
