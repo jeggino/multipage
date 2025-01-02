39772c35c61d796c54072839929eaeb273f690f6
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-02 11:03:13 +0100

    Update 📌_Voeg_een_waarneming_in.py

--- page/📌_Voeg_een_waarneming_in.py
+++ page/📌_Voeg_een_waarneming_in.py
@@ -66,7 +66,7 @@ def map():
     
     folium.TileLayer('OpenStreetMap',overlay=False,show=True,name="Stratenkaart").add_to(m)
     folium.TileLayer(tiles='CartoDB Positron',
-                 attr='XXX Mapbox Attribution',overlay=False,show=False,name="Witte kaart").add_to(m)
+                 attr='XXX Mapbox Attribution',overlay=False,show=False,name="Witte contrastkaart").add_to(m)
     folium.TileLayer(tiles='https://api.mapbox.com/styles/v1/jeggino/cm2vtvb2l000w01qz9wet0mv9/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiamVnZ2lubyIsImEiOiJjbHdscmRkZHAxMTl1MmlyeTJpb3Z2eHdzIn0.N9TRN7xxTikk235dVs1YeQ',
                      attr='XXX Mapbox Attribution',overlay=False,show=False,name="Satellietkaart").add_to(m)
     
