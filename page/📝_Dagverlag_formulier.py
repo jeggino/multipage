e6d56b8d80659e8d88037f6c7f9f083c856e11cd
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-14 10:12:37 +0100

    Update 📝_Dagverlag_formulier.py

--- page/📝_Dagverlag_formulier.py
+++ page/📝_Dagverlag_formulier.py
@@ -69,7 +69,7 @@ opdracht = st.session_state.project['opdracht']
 st.title(f'{project}')
 st.header(f'Opdracht: **{opdracht}**',divider=True)
 
-selected = option_menu(None,["Formulier", 'Databank'], icons=['house', 'Tasks'],orientation="horizontal",)
+selected = option_menu(None,["", ''], icons=['bi-pen-fill', 'bi-database'],orientation="horizontal",)
 
 if selected == "Formulier":
     with st.form("my_form", clear_on_submit=True,border=True):
@@ -115,29 +115,30 @@ if selected == "Formulier":
             # st.switch_page("page/🧭_navigatie.py")
         "---"
 
-# elif selected == 'Databank':
-#     geometry_file = f"geometries/{st.session_state.project["project_name"]}.geojson" 
-#     gdf_areas = gpd.read_file(geometry_file)
-#     gebied_id_list = gdf_areas['Gebied'].unique()
-#     gebied_id = st.sidebar.selectbox("Gebied",gebied_id_list,index=None)
-#     doel = st.sidebar.selectbox('Doel',('Kraamverblijf','Winterverblijf','Paarverblijf'),index=None)
-#     if doel:
-#         df_filter = df_old[(df_old['doel']==doel)&(df_old['gebied_id']==gebied_id)]
-#         date_options = df_filter['datum'].unique()
-#         date_input = st.sidebar.selectbox('Datum',date_options,index=None)
-#     try:
-#         df_filter_2 = df_filter[(df_filter['datum']==date_input)].reset_index(drop=True)
-#         st.write('Waarnemer: 'df_filter_2.loc[0,'waarnemer'])
-#         st.write('Start time: 'df_filter_2.loc[0,'start_time'])
-#         st.write('Eind time: ' df_filter_2.loc[0,'eind_time'])
-#         st.write('temperatuur: 'df_filter_2.loc[0,'temperatuur'])
-#         st.write('bewolking: 'df_filter_2.loc[0,'bewolking'])
-#         st.write('neerslag: 'df_filter_2.loc[0,'neerslag'])
-#         st.write('windkrcht: 'df_filter_2.loc[0,'windkrcht'])
-#         st.write('windrichting: 'df_filter_2.loc[0,'windrichting'])
-#         st.write('opmerking: 'df_filter_2.loc[0,'opmerking'])
-#     except:
-#         st.stop()
+elif selected == 'Databank':
+    st.image('https://th.bing.com/th/id/R.9b05c7a5db7a093407c47efc77073a34?rik=IElQBmbi8QoEpA&riu=http%3a%2f%2fkinderscientific.com%2fwp-content%2fuploads%2f2018%2f06%2fWork-in-Progress.jpg&ehk=Udc6o7K7mopYeuVxHWM7qb%2f%2f6udgrt%2fp%2bYwVywZTQCc%3d&risl=&pid=ImgRaw&r=0')
+    # geometry_file = f"geometries/{st.session_state.project["project_name"]}.geojson" 
+    # gdf_areas = gpd.read_file(geometry_file)
+    # gebied_id_list = gdf_areas['Gebied'].unique()
+    # gebied_id = st.sidebar.selectbox("Gebied",gebied_id_list,index=None)
+    # doel = st.sidebar.selectbox('Doel',('Kraamverblijf','Winterverblijf','Paarverblijf'),index=None)
+    # if doel:
+    #     df_filter = df_old[(df_old['doel']==doel)&(df_old['gebied_id']==gebied_id)]
+    #     date_options = df_filter['datum'].unique()
+    #     date_input = st.sidebar.selectbox('Datum',date_options,index=None)
+    # try:
+    #     df_filter_2 = df_filter[(df_filter['datum']==date_input)].reset_index(drop=True)
+    #     st.write('Waarnemer: 'df_filter_2.loc[0,'waarnemer'])
+    #     st.write('Start time: 'df_filter_2.loc[0,'start_time'])
+    #     st.write('Eind time: ' df_filter_2.loc[0,'eind_time'])
+    #     st.write('temperatuur: 'df_filter_2.loc[0,'temperatuur'])
+    #     st.write('bewolking: 'df_filter_2.loc[0,'bewolking'])
+    #     st.write('neerslag: 'df_filter_2.loc[0,'neerslag'])
+    #     st.write('windkrcht: 'df_filter_2.loc[0,'windkrcht'])
+    #     st.write('windrichting: 'df_filter_2.loc[0,'windrichting'])
+    #     st.write('opmerking: 'df_filter_2.loc[0,'opmerking'])
+    # except:
+    #     st.stop()
 
     
     
