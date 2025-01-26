3bd8dcb784000a30e94dffd7fdd86f094c217320
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-26 14:19:16 +0100

    Update statistik.py

--- page/statistik.py
+++ page/statistik.py
@@ -62,7 +62,8 @@ elif selected == "Data":
         df_point = pd.DataFrame(rows_points.data)
         df_download_points = df_point[df_point['project']==project]
         if len(df_download_points)==0:
-            col1.stop()
+            col1.image('https://t4.ftcdn.net/jpg/04/72/65/73/360_F_472657366_6kV9ztFQ3OkIuBCkjjL8qPmqnuagktXU.jpg',
+                        width=450)
         else:
             df_download_points
             st.download_button(label="Download waarnemingen",data=df_download_points.to_csv().encode("utf-8"),
