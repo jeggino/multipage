8b3635a5bda8db115a85d03822298eaaf078c282
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-26 14:35:45 +0100

    Update statistik.py

--- page/statistik.py
+++ page/statistik.py
@@ -69,13 +69,13 @@ elif selected == "Data":
             st.download_button(label="Download waarnemingen",data=df_download_points.to_csv().encode("utf-8"),
                                file_name="waarnemingen.csv",mime="text/csv", use_container_width=True) 
     elif selection=="Dagverlagen":
-        with st.container(border=True):
-            try:
-                
-                rows_dagverslagen = supabase.table("df_dagverslagen").select("*").execute()
-                df_dagverslagen = pd.DataFrame(rows_dagverslagen.data)                
-                df_download_dagverslagen = df_dagverslagen[(df_dagverslagen['project']==project) & (df_dagverslagen['opdracht']==opdracht)]
-
+        try:
+            rows_dagverslagen = supabase.table("df_dagverslagen").select("*").execute()
+            df_dagverslagen = pd.DataFrame(rows_dagverslagen.data)                
+            df_download_dagverslagen = df_dagverslagen[(df_dagverslagen['project']==project) & (df_dagverslagen['opdracht']==opdracht)]
+            st.download_button(label="downloaden voor alle gebieden",data=df_download_dagverslagen.to_csv().encode("utf-8"),
+                               file_name="dagverslagen.csv",mime="text/csv", use_container_width=True)
+            with st.container(border=True):
                 option_areas_filter = col1.selectbox(
                     "Selecteer een gebied",
                     df_download_dagverslagen['gebied_id'].unique(),
@@ -118,7 +118,6 @@ elif selected == "Data":
         
                 except:
                     pass
-                col1.download_button(label="downloaden voor alle gebieden",data=df_download_dagverslagen.to_csv().encode("utf-8"),file_name="dagverslagen.csv",mime="text/csv", use_container_width=True)
-            except:
-                st.image('https://t4.ftcdn.net/jpg/04/72/65/73/360_F_472657366_6kV9ztFQ3OkIuBCkjjL8qPmqnuagktXU.jpg',
-                        width=450)
+        except:
+            st.image('https://t4.ftcdn.net/jpg/04/72/65/73/360_F_472657366_6kV9ztFQ3OkIuBCkjjL8qPmqnuagktXU.jpg',
+                    width=450)
