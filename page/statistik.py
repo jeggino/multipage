c06d3cfa60575830f125dab4dc3a10b433f95d80
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-01-26 09:35:05 +0100

    Update statistik.py

--- page/statistik.py
+++ page/statistik.py
@@ -44,8 +44,8 @@ opdracht = st.session_state.project['opdracht']
 
 selected = option_menu(None,["Numbers", 'Data'], icons=['bi-pen-fill', 'bi-database'],orientation="horizontal",)
 
-# if selected == "Numbers":
-#     st.image('https://th.bing.com/th/id/R.9b05c7a5db7a093407c47efc77073a34?rik=IElQBmbi8QoEpA&riu=http%3a%2f%2fkinderscientific.com%2fwp-content%2fuploads%2f2018%2f06%2fWork-in-Progress.jpg&ehk=Udc6o7K7mopYeuVxHWM7qb%2f%2f6udgrt%2fp%2bYwVywZTQCc%3d&risl=&pid=ImgRaw&r=0')
+if selected == "Numbers":
+    st.image('https://th.bing.com/th/id/R.9b05c7a5db7a093407c47efc77073a34?rik=IElQBmbi8QoEpA&riu=http%3a%2f%2fkinderscientific.com%2fwp-content%2fuploads%2f2018%2f06%2fWork-in-Progress.jpg&ehk=Udc6o7K7mopYeuVxHWM7qb%2f%2f6udgrt%2fp%2bYwVywZTQCc%3d&risl=&pid=ImgRaw&r=0')
 
     
 # elif selected == "Data":
