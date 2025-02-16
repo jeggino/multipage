01142a4a97b8150f13e75ff683fc9ae0aa47be45
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-16 18:39:58 +0100

    Update 📝_Dagverlag_formulier.py

--- page/📝_Dagverlag_formulier.py
+++ page/📝_Dagverlag_formulier.py
@@ -58,16 +58,10 @@ def insert_dagverslag(waarnemer,project,opdracht,gebied_id,doel,datum,start_time
 def update_dagverslag(key,waarnemer,project,opdracht,gebied_id,temperatuur,opmerking):
     
     if opdracht == 'Vleermuizen':
-        if project != "Overig":
-            doel = st.selectbox('Doel',('Kraamverblijf','Winterverblijf','Paarverblijf'))
-        else:
-            doel = st.selectbox('Doel',('Overig','Kraamverblijf','Winterverblijf','Paarverblijf'))
-            
+        doel = st.selectbox('Doel',('Kraamverblijf','Winterverblijf','Paarverblijf'))
+      
     elif opdracht == 'Vogels':
-        if project != "Overig":
-            doel = st.selectbox('Doel',['Gierzwaluw','Huismus'])
-        else:
-            doel = st.selectbox('Doel',['Overig'] + BIRD_NAMES)
+        doel = st.selectbox('Doel',['Gierzwaluw','Huismus'])
             
     datum = st.date_input("Datum","today")       
     two_hours_from_now = datetime.now() + timedelta(hours=1)
@@ -80,18 +74,18 @@ def update_dagverslag(key,waarnemer,project,opdracht,gebied_id,temperatuur,opmer
     windkracht = st.number_input("Windkracht (Bft)",key='windkracht', min_value=1)
     windrichting = st.selectbox("Windrichting",("Noord", "Noordoost", "Oost", "Zuidoost","Zuid","Zuidwest","West","Noordwest"))     
     opmerking = st.text_area("", placeholder="Vul hier een opmerking in ...",value=opmerking)
-    # if st.button("**Update**",use_container_width=True):
-    #     data = {"waarnemer":waarnemer,"project":project,"opdracht":opdracht,"gebied_id":gebied_id,'doel':doel,"datum":datum,
-    #              "start_time":start_time,"eind_time":eind_time,"temperatuur":temperatuur, "bewolking":bewolking,
-    #              "neerslag":neerslag,"windkracht":windkracht,"windrichting":windrichting,"opmerking":opmerking
-    #     response = (
-    #         supabase.table("df_dagverslagen")
-    #         .update(data)
-    #         .eq("key", key)
-    #         .execute()
-    #         )
+    if st.button("**Update**",use_container_width=True):
+        data = {"waarnemer":waarnemer,"project":project,"opdracht":opdracht,"gebied_id":gebied_id,'doel':doel,"datum":datum,
+                 "start_time":start_time,"eind_time":eind_time,"temperatuur":temperatuur, "bewolking":bewolking,
+                 "neerslag":neerslag,"windkracht":windkracht,"windrichting":windrichting,"opmerking":opmerking
+        response = (
+            supabase.table("df_dagverslagen")
+            .update(data)
+            .eq("key", key)
+            .execute()
+            )
         
-    #     st.rerun()
+        st.rerun()
 
     
 #---DATASET---
