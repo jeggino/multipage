8ec9d04d338fc317c2136948713c68864db1a36d
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-16 17:52:55 +0100

    Update 📝_Dagverlag_formulier.py

--- page/📝_Dagverlag_formulier.py
+++ page/📝_Dagverlag_formulier.py
@@ -54,71 +54,57 @@ def insert_dagverslag(waarnemer,project,opdracht,gebied_id,doel,datum,start_time
             .execute()
         )
 
-# @st.dialog(" ")
-# def update_dagverslag(id,df):
+@st.dialog(" ")
+def update_dagverslag(key,waarnemer,project,opdracht,temperatuur,opmerking):
     
-#   df_filter = df[df["key"]==id].reset_index(drop=True)
-
-
-#   id_date = df_filter['datum'][0]
-#   # id_time = df_filter['time'][0]
-#   id_lat = df_filter['lat'][0]
-#   id_lng = df_filter['lng'][0]
-#   id_waarnemer = df_filter['waarnemer'][0]
-#   id_key = df_filter['key'][0]
-#   id_soortgroup = df_filter['soortgroup'][0]
-#   id_geometry_type = df_filter['geometry_type'][0]
-#   id_coordinates = df_filter['coordinates'][0]
-#   id_project = df_filter['project'][0]
-#   id_functie = df_filter['functie'][0]
-#   id_gedrag = df_filter['gedrag'][0]
-#   id_verblijf = df_filter['verblijf'][0]
-#   id_sp = df_filter['sp'][0]
-#   id_aantal = df_filter['aantal'][0]
-#   id_opmerking = df_filter['opmerking'][0]
-  
-#   datum = st.date_input("Datum",id_date)
-#   # nine_hours_from_now = datetime.now() + timedelta(hours=2)
-#   time = st.time_input("Tijd" )
-  
-#   if st.session_state.project['opdracht'] == 'Vleermuizen':
-
-#     sp = st.selectbox("Soort", BAT_NAMES,index=BAT_NAMES.index(id_sp))
- 
-#     if output["last_active_drawing"]["geometry"]["type"] == 'Polygon':
-#         gedrag = None
-#         functie = st.selectbox("Functie", GEBIED_OPTIONS,index=GEBIED_OPTIONS.index(id_functie))
-#         verblijf = None
-#     else:
-#         gedrag = st.selectbox("Gedrag", BAT_BEHAVIOURS,index=BAT_BEHAVIOURS.index(id_gedrag)) 
-#         functie = st.selectbox("Functie", BAT_FUNCTIE,index=BAT_FUNCTIE.index(id_functie))
-#         verblijf = st.selectbox("Verblijf", BAT_VERBLIJF,index=BAT_VERBLIJF.index(id_verblijf)) 
-
-#   elif st.session_state.project['opdracht'] == 'Vogels':
-  
-#     sp = st.selectbox("Soort", BIRD_NAMES,index=BIRD_NAMES.index(id_sp))
-#     gedrag = st.selectbox("Gedrag", BIRD_BEHAVIOURS,index=BIRD_BEHAVIOURS.index(id_gedrag)) 
-#     functie = st.selectbox("Functie", BIRD_FUNCTIE,index=BIRD_FUNCTIE.index(id_functie)) 
-#     verblijf = st.selectbox("Verblijf", BIRD_VERBLIJF,index=BIRD_VERBLIJF.index(id_verblijf)) 
+    if opdracht == 'Vleermuizen':
+        if project != "Overig":
+            doel = st.selectbox('Doel',('Kraamverblijf','Winterverblijf','Paarverblijf'))
+        else:
+            doel = st.selectbox('Doel',('Overig','Kraamverblijf','Winterverblijf','Paarverblijf'))
+            
+    elif opdracht == 'Vogels':
+        if project != "Overig":
+            doel = st.selectbox('Doel',['Gierzwaluw','Huismus'])
+        else:
+            doel = st.selectbox('Doel',['Overig'] + BIRD_NAMES)
+            
+    try: 
+        geometry_file = f"geometries/{st.session_state.project["project_name"]}.geojson" 
+        gdf_areas = gpd.read_file(geometry_file)
+        gebied_id_list = gdf_areas['Gebied'].unique()
+        gebied_id = st.selectbox("Gebied",gebied_id_list,index=None)
+    except:
+        gebied_id = "---"
+    datum = st.date_input("Datum","today")       
+    two_hours_from_now = datetime.now() + timedelta(hours=1)
+    four_hours_from_now = datetime.now() + timedelta(hours=3)
+    start_time = st.time_input("Start tijd", two_hours_from_now)
+    eind_time = st.time_input("Eind tijd", four_hours_from_now)               
+    temperatuur = st.number_input("Temperatuur",key='temperatuur', min_value=0,value=temperatuur)
+    bewolking = st.selectbox("Bewolking",("Onbewolkt (<10%)", "Halfbewolkt (10-80%)", "Bewolkt (>80%)"))
+    neerslag = st.selectbox("Neerslag",("Droog", "Nevel/mist", "Motregen", "Regen"))
+    windkracht = st.number_input("Windkracht (Bft)",key='windkracht', min_value=1)
+    windrichting = st.selectbox("Windrichting",("Noord", "Noordoost", "Oost", "Zuidoost","Zuid","Zuidwest","West","Noordwest"))     
+    opmerking = st.text_area("", placeholder="Vul hier een opmerking in ...",value=opmerking)
+    
+    if st.form_submit_button("**Gegevens opslaan**",use_container_width=True):
+        if gebied_id == None:
+            st.error("Selecteer een gebied, alstublieft",icon="⚠️")
+            st.stop()
+        data = {"waarnemer":waarnemer,"project":project,"opdracht":opdracht,"gebied_id":gebied_id,'doel':doel,"datum":datum,
+                 "start_time":start_time,"eind_time":eind_time,"temperatuur":temperatuur, "bewolking":bewolking,
+                 "neerslag":neerslag,"windkracht":windkracht,"windrichting":windrichting,"opmerking":opmerking
+        response = (
+            supabase.table("df_dagverslagen")
+            .update(data)
+            .eq("key", key)
+            .execute()
+            )
+        
+        st.rerun()
 
     
-#   aantal = st.number_input("Aantal", min_value=1,value=int(id_aantal))    
-#   opmerking = st.text_area("", placeholder="Vul hier een opmerking in ...",value=id_opmerking)
-
-#   if st.button("**Update**",use_container_width=True):
-      
-#     data = {"key":id_key,"waarnemer":id_waarnemer,"datum":str(datum),"time":str(time),"soortgroup":id_soortgroup, "aantal":aantal,
-#                    "sp":sp, "gedrag":gedrag, "functie":functie, "verblijf":verblijf,
-#                    "geometry_type":id_geometry_type,"lat":id_lat,"lng":id_lng,"opmerking":opmerking,"coordinates":id_coordinates,"project":id_project}
-      
-#     response = (
-#         supabase.table("df_observations")
-#         .update(data)
-#         .eq("key", id)
-#         .execute()
-#     )
-
-#     st.rerun()
 #---DATASET---
 
 
@@ -250,15 +236,22 @@ elif selected == 'Data':
                         st.info('Selecteer een rij om de Dagverlageninformatie te krijgen')
                     else:
                         key = df_filter.loc[event.selection['rows'][0],'key']
-                        st.write(f"**:blue[Samensteller:]** {df_filter.loc[event.selection['rows'][0],'waarnemer']}")
+                        project_id = df_filter.loc[event.selection['rows'][0],'project']
+                        opdracht_id = df_filter.loc[event.selection['rows'][0],'opdracht']
+                        waarnemer_id = df_filter.loc[event.selection['rows'][0],'waarnemer']
+                        st.write(f"**:blue[Samensteller:]** {waarnemer_id}")
                         st.write(f"**:blue[Begin tijd:]** {df_filter.loc[event.selection['rows'][0],'start_time']}")
                         st.write(f"**:blue[Eind tijd:]** {df_filter.loc[event.selection['rows'][0],'eind_time']}")
-                        st.write(f"**:blue[Temperatuur:]** {df_filter.loc[event.selection['rows'][0],'temperatuur']}")
+                        temperatuur_id = df_filter.loc[event.selection['rows'][0],'temperatuur']
+                        st.write(f"**:blue[Temperatuur:]** {temperatuur_id}")
                         st.write(f"**:blue[Bewolking:]** {df_filter.loc[event.selection['rows'][0],'bewolking']}")
                         st.write(f"**:blue[Neerslag:]** {df_filter.loc[event.selection['rows'][0],'neerslag']}")
                         st.write(f"**:blue[Windkracht:]** {df_filter.loc[event.selection['rows'][0],'windkracht']}")
                         st.write(f"**:blue[Windrichting:]** {df_filter.loc[event.selection['rows'][0],'windrichting']}")
-                        st.write(f"{df_filter.loc[event.selection['rows'][0],'opmerking']}")
+                        opmerking_id = df_filter.loc[event.selection['rows'][0],'opmerking']
+                        st.write(f"{opmerking_id}")
+                        if st.button("Dagverslag bijwerken",use_container_width=True): 
+                            update_dagverslag(key,waarnemer_id,project_id,opdracht_id,temperatuur_id,opmerking_id)
 
             except:
                 pass
