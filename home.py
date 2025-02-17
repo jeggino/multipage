3675ff69e2f8ba222fd708be6e7a3ccb021504cf
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-17 09:02:08 +0100

    Update home.py

--- home.py
+++ home.py
@@ -119,10 +119,10 @@ def logOut_project():
 
 #---APP---
 page_1 = st.Page("page/🧭_navigatie.py", title="Navigatie",icon=":material/explore:" )
-page_2 = st.Page("page/📌_Voeg_een_waarneming_in.py", title="Voeg een waarneming in",icon="📌" )
-page_3 = st.Page("page/📝_Dagverlag_formulier.py", title="Dagverlag formulier",icon="📝" )
-page_4 = st.Page("page/📊_ Statistik.py", title="Videos/Fotos",icon="📹" )
-page_5 = st.Page("page/statistik.py", title="Statistik",icon="📊" )
+page_2 = st.Page("page/📌_Voeg_een_waarneming_in.py", title="Voeg een waarneming in",icon=":material/pin_drop:" )
+page_3 = st.Page("page/📝_Dagverlag_formulier.py", title="Dagverlag formulier",icon=":material/edit_note:" )
+page_4 = st.Page("page/📊_ Statistik.py", title="Videos/Fotos",icon=":material/photo-camera:" )
+page_5 = st.Page("page/statistik.py", title="Statistik",icon=":material/bar_chart:" )
 
 
 #---APP---
