326d681da1f9becdac7b164a49d57811c26cf8c8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-25 21:44:06 +0100

    Update 📊_ Statistik.py

--- page/📊_ Statistik.py
+++ page/📊_ Statistik.py
@@ -31,8 +31,11 @@ st.logo(IMAGE,  link=None, size="large",icon_image=IMAGE)
 try:
     for key in media_dict[st.session_state.project['project_name']][st.session_state.project['opdracht']]:
         with st.container(border=True):
-            st.video(key,loop=True, autoplay=False, muted=True)
-            st.caption(media_dict[st.session_state.project['project_name']][st.session_state.project['opdracht']][key])
+            try:
+                st.video(key,loop=True, autoplay=False, muted=True)
+                st.caption(media_dict[st.session_state.project['project_name']][st.session_state.project['opdracht']][key])
+            except:
+                st.image(key)
 except:
     st.image('https://cf.ltkcdn.net/travel/images/std/198833-425x283-Not-There-Yet.jpg')
    
