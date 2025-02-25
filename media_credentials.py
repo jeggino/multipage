e4a80d0f0d86a70c3d82d1e64b55623d23b4f687
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-25 22:26:56 +0100

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -1,7 +1,7 @@
 media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube.com/watch?v=OKEaI1mFAhc":"",
                                                             'https://www.youtube.com/watch?v=6qO1mpw8Xow':'Laatvliegers foerageren boven fietspad te Lies, Terschelling. Lantaarnpaal trekt waarschijnlijk prooien aan en de laatvliegers maken daar gretig gebruik van.',
                                                             },
-                                                   "Photos":{}
+                                                   "Photos":{"https://www.flickr.com/photos/67492897@N07/47838901471/in/album-72157708407869275":"s"}
                                                  },
                                    'Vogels':{"Videos":{},
                                              "Photos":{}
