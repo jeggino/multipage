0dfaf6288a3c3a49312bcddcb17f5675c4a2e5cc
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-02-25 23:07:45 +0100

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -1,8 +1,7 @@
 media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube.com/watch?v=OKEaI1mFAhc":" ",
                                                             'https://www.youtube.com/watch?v=6qO1mpw8Xow':'Laatvliegers foerageren boven fietspad te Lies, Terschelling. Lantaarnpaal trekt waarschijnlijk prooien aan en de laatvliegers maken daar gretig gebruik van.',
                                                             },
-                                                   "Photos":{'<a data-flickr-embed="true" href="https://www.flickr.com/photos/67492897@N07/52147404647/in/dateposted/" title="_MG_0069_edited"><img src="https://live.staticflickr.com/65535/52147404647_fee5998d79_w.jpg" width="400" height="400" alt="_MG_0069_edited"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>':"",
-                                                             '<a href="https://www.linkedin.com/posts/daan-van-der-elsken-2515b933_elsken-ecologie-gaat-smp-terschelling-verzorgen-activity-7300185085973024769-upFZ?utm_source=share&utm_medium=member_desktop&rcm=ACoAAARrlQYBooU5fgR-_Bs4xG7fLfcQ7K02iTc"><img src="https://media.licdn.com/dms/image/v2/D4E22AQGF468ryfrthg/feedshare-shrink_800/B4EZU93l9_HUAk-/0/1740499748627?e=1743638400&v=beta&t=pOEFwv4cMSXi04zTmShj1iiG1uZzhNXajhT4_YivLN4" width="400" height="225" alt="173321538_312506646958826_9060645562680390797_n"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>':'dd'
+                                                   "Photos":{'https://www.ecoloogterschelling.nl/wp-content/uploads/2023/09/gewone-dwergvleermuis-invliegend-kolonie.jpeg':''
                                                             }
                                                  },
                                    'Vogels':{"Videos":{},
