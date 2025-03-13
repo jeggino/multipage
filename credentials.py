5e042a3b6ebf13b4546bb3f6d9440be6db3265a6
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-03-13 10:27:03 +0100

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -101,6 +101,7 @@ DICTIONARY_PROJECTS = {"Overig":["Vogels","Vleermuizen"],
                        'Beatrixlaan-1-31_Uithoorn':["Vleermuizen","Vogels"],
                        'Middelweg-34_Starnmeer':["Vogels"],
                        'Kerkebuurt-39_Marken':["Vogels"],
-                       'HollandsEnd-22_Ankeveen': ["Vleermuizen","Vogels"]
+                       'HollandsEnd-22_Ankeveen': ["Vleermuizen","Vogels"],
+                       'SMPs-Hoorn':["Vleermuizen","Vogels"],
                        
                       }
