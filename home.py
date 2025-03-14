37afca7786e011d89a4c3ab7665bb82cb08ceeb3
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-03-14 14:42:16 +0100

    Update home.py

--- home.py
+++ home.py
@@ -143,7 +143,7 @@ if st.session_state.login['type'] == 'user':
     if st.session_state.project['project_name'] != 'Admin':
         if st.session_state.project['project_name'] == 'Overig':
             if st.session_state.project['auto_start'] == False:
-                pg = st.navigation([page_1,page_2,page_4])
+                pg = st.navigation([page_1,page_2,page_5])
             if st.session_state.project['auto_start'] == True:
                 pg = st.navigation([page_1,page_2])
         else:
