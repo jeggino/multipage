0b46e96c18b434c330311c83df2a1fd1652f88fe
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-03-14 17:48:14 +0100

    Update statistik.py

--- page/statistik.py
+++ page/statistik.py
@@ -89,7 +89,9 @@ col1, col2 = st.columns([0.2,0.8],gap="large", vertical_alignment="top", border=
 
 with col2:
     st.altair_chart(chart, use_container_width=False,theme=None)
-col1.dataframe(df.groupby('functie')['value'].sum(),use_container_width=True)
+col1.dataframe(df.groupby('functie')['value'].sum(),use_container_width=True,
+              column_config={'functie':'Functie',
+                             'value':'Aantal'})
     
 st.download_button(label="Downloaden alle waarnemingen",data=df_download_points.to_csv().encode("utf-8"),
                    file_name=f"{project}_{opdracht}_Waarnemingen.csv",mime="text/csv", use_container_width=False)
