71ab6e223bf7f1f4ef7f114713b20bdb704f8c88
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-04-01 15:47:10 +0200

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -12,7 +12,7 @@ media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube
                                                           "Photos":{}
                                                          },
                                            'Vogels':{"Video":{},
-                                                     "Photos":{'<a data-flickr-embed="true" href="https://www.flickr.com/photos/67492897@N07/54423034002/in/dateposted/" title="IMG_5819_edited"><img src="https://live.staticflickr.com/65535/54423034002_a45a3e9e3f_w.jpg" width="400" height="400" alt="IMG_5819_edited"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>':''}
+                                                     "Photos":{'<a data-flickr-embed="true" href="https://www.flickr.com/photos/67492897@N07/54423034002/in/dateposted/" title="IMG_5819_edited"><img src="https://live.staticflickr.com/65535/54423034002_a45a3e9e3f_w.jpg" width="400" height="400" alt="IMG_5819_edited"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>':'House number 27 located at the corner of the street near the gutter.'}
                                                     }
                                           }
              }
