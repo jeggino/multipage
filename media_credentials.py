7cdadf64908f800b383b10ffd2b17ca5658464e8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-04-01 15:31:35 +0200

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -8,4 +8,11 @@ media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube
                                              "Photos":{}
                                             }
                                   },
+              'Beatrixlaan-1-31_Uithoorn':{'Vleermuizen':{"Videos":{},
+                                                          "Photos":{}
+                                                         },
+                                           'Vogels':{"Video":{},
+                                                     "Photos":{'<a data-flickr-embed="true" href="https://www.flickr.com/photos/67492897@N07/54423034002/in/dateposted/" title="IMG_5819_edited"><img src="https://live.staticflickr.com/65535/54423034002_a45a3e9e3f_w.jpg" width="400" height="400" alt="IMG_5819_edited"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>'}
+                                                    }
+                                          }
              }
