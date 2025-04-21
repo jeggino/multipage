d8055a853e572ca72da716ff9b7b0b808449fad1
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-04-21 13:52:54 +0200

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -824,11 +824,12 @@ if st.session_state.login['type'] == 'user':
             name = f"{id}"
         except:
             try:
+                id = str(output["last_active_drawing"]['geometry']['coordinates'][0][0][0])+str(output["last_active_drawing"]['geometry']['coordinates'][0][0][1])
+                name = f"{id}"    
+            except:
+
                 id = str(output["last_active_drawing"]['geometry']['coordinates'][0][0])+str(output["last_active_drawing"]['geometry']['coordinates'][0][1])
                 name = f"{id}"
-            except:
-                id = str(output["last_active_drawing"]['geometry']['coordinates'][0][0][0])+str(output["last_active_drawing"]['geometry']['coordinates'][0][0][1])
-                name = f"{id}"       
 
 
         
