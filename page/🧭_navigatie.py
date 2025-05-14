2739bc8dac8caea778f625fad96201718175d386
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-05-14 14:40:05 +0200

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -108,8 +108,10 @@ def legend(species_colors_dict,dragable=True):
         <li><span class="fa fa-venus-double" style="color:grey" opacity: 0.75;'></span>Kraamverblijf</li>
         <li><span class="fa fa-snowflake" style="color:grey" opacity: 0.75;'></span>Winterverblijf</li>
         <li><span class="fa fa-heart" style="color:grey" opacity: 0.75;'></span>Paarverblijf</li>
+        <li>-</li>
         <li><span class="fa fa-box-archive" style="color:grey" opacity: 0.75;'></span>Vleermuiskast</li>
         <li><span class="fa fa-tower-broadcast" style="color:grey" opacity: 0.75;'></span>Zender</li>
+        <li>-</li>
         <li><span class="fa-solid fa-clone" style="color:grey" opacity: 0.75;'></span>Foerageergebied</li>
         <li><span class="fa-regular fa-clone" style="color:grey" opacity: 0.75;'></span>Baltsterritorium</li>
         <li><span class="fa fa-minus" style="color:grey" opacity: 0.75;'></span>Vliegroute</li>
