37bdf81c0c02426a654303f993f37609469da682
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-05-14 14:13:52 +0200

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -108,6 +108,7 @@ def legend(species_colors_dict,dragable=True):
         <li><span class="fa fa-venus-double" style="color:grey" opacity: 0.75;'></span>Kraamverblijf</li>
         <li><span class="fa fa-snowflake" style="color:grey" opacity: 0.75;'></span>Winterverblijf</li>
         <li><span class="fa fa-heart" style="color:grey" opacity: 0.75;'></span>Paarverblijf</li>
+        <li><span class="fa fa-box-archive" style="color:grey" opacity: 0.75;'></span>Vleermuiskast</li>
         <li><span class="fa fa-tower-broadcast" style="color:grey" opacity: 0.75;'></span>Zender</li>
         <li><span class="fa-solid fa-clone" style="color:grey" opacity: 0.75;'></span>Foerageergebied</li>
         <li><span class="fa-regular fa-clone" style="color:grey" opacity: 0.75;'></span>Baltsterritorium</li>
@@ -722,6 +723,7 @@ map_fuction = {'paarverblijfplaats':'heart',
                               'zomerverblijfplaats':'star',
                               'kraamverblijfplaats':'venus-double',
                               'winterverblijfplaats':'snowflake',
+               'vleermuiskast':'fa-box-archive',
                'zender':'tower-broadcast',
                                             'vogel waarneming':'binoculars',
                                              'mogelijke nestlocatie':'fa-regular fa-circle-question',
