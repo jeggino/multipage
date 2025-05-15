b929492772789cde2bafb63a5952873f14304569
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-05-15 16:05:34 +0200

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -104,6 +104,7 @@ def legend_dialog(species_colors_dict):
       <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     </head>
     <body>
+      <ul>
         <li><strong>Sorten</strong></li>
     
         {legend_temp}
