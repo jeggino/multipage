82e864db77a86d7efbcffb493ac74a984713ee18
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-05-18 07:37:49 +0200

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -661,10 +661,10 @@ elif st.session_state.project['project_name'] == 'Admin':
     df_dict = df_2.copy()
 
 else:
-    if st.session_state.project["project_name"]=='SMPs-ZuidOost':
-        df_2 = df_point[(df_point['project']==st.session_state.project['project_name'])&(df_point['sp']=='Gierzwaluw'])]
-    else:
-        df_2 = df_point[(df_point['project']==st.session_state.project['project_name'])&(df_point['soortgroup']==st.session_state.project['opdracht'])]
+    # if st.session_state.project["project_name"]=='SMPs-ZuidOost':
+    #     df_2 = df_point[(df_point['project']==st.session_state.project['project_name'])&(df_point['sp']=='Gierzwaluw'])]
+    # else:
+    df_2 = df_point[(df_point['project']==st.session_state.project['project_name'])&(df_point['soortgroup']==st.session_state.project['opdracht'])]
     df_overig = df_point[(df_point['project']!=st.session_state.project['project_name']) & (df_point['soortgroup']==st.session_state.project['opdracht'])]
     df_dict = df_point[df_point['soortgroup']==st.session_state.project['opdracht']]
 
