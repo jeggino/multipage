865f13f292d609f3e7149a8c2ded3eef4e5884b0
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-05-20 06:26:44 +0200

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -1,6 +1,6 @@
 media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube.com/watch?v=OKEaI1mFAhc":" ",
                                                             'https://www.youtube.com/watch?v=6qO1mpw8Xow':'Laatvliegers foerageren boven fietspad te Lies, Terschelling. Lantaarnpaal trekt waarschijnlijk prooien aan en de laatvliegers maken daar gretig gebruik van.',
-                                                            },
+                                                            'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp//signal-2025-04-27-105501.mp4':'Franschenland 24, 26-04-2025'},
                                                    "Photos":{'<img src="https://www.ecoloogterschelling.nl/wp-content/uploads/2023/09/gewone-dwergvleermuis-invliegend-kolonie.jpeg" width="400" />':'',                                                            
                                                             }
                                                  },
