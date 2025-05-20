7e9c68190b5475d67752eb0f91bd65008584bbf8
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-05-20 08:12:18 +0200

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -79,7 +79,6 @@ def legend_dialog_bats(species_colors_dict):
     b = f"""
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <div style="background-color: white; padding: 20px; border-radius: 5px; font-size:85%;">
-            <h2><strong>Sorten</strong></h2>
             {legend_temp}
             <hr>
             <h2><strong>Functie</strong></h2>
