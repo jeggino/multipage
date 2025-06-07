4c375fd5c7603539106133fcfd644e190ccb2e80
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-06-07 12:51:19 +0200

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -4,21 +4,21 @@ media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube
                                                    "Photos":{'<img src="https://www.ecoloogterschelling.nl/wp-content/uploads/2023/09/gewone-dwergvleermuis-invliegend-kolonie.jpeg" width="400" />':'',                                                            
                                                             }
                                                  },
-                                   'Vogels':{"Video":{},
+                                   'Vogels':{"Videos":{},
                                              "Photos":{}
                                             }
                                   },
               'Beatrixlaan-1-31_Uithoorn':{'Vleermuizen':{"Videos":{},
                                                           "Photos":{}
                                                          },
-                                           'Vogels':{"Video":{},
+                                           'Vogels':{"Videos":{},
                                                      "Photos":{'<a data-flickr-embed="true" href="https://www.flickr.com/photos/67492897@N07/54423034002/in/dateposted/" title="IMG_5819_edited"><img src="https://live.staticflickr.com/65535/54423034002_a45a3e9e3f_w.jpg" width="400" height="400" alt="IMG_5819_edited"/></a><script async src="//embedr.flickr.com/assets/client-code.js" charset="utf-8"></script>':'House number 27 located at the corner of the street near the gutter.'}
                                                     }
                                           },
               'SMPs-ZuidOost':{'Vleermuizen':{"Videos":{},
                                               "Photos":{}
                                                          },
-                               'Vogels':{"Video":{'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp//WhatsApp%20Video%202025-06-07%20at%2012.18.50_f618cd26.mp4':'Daalwijk 907: invliegende gierzwaluw in spleet onder de dakrand bij de bovenste etage van het flatgebouw. Dit gebeurde tussen 21.45 en 22.30 op een groot aantal verschillende plekken (tussen de 10 en 15 locaties) over de gehele lengte van deze zijde van de flat.'},
+                               'Vogels':{"Videos":{'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp//WhatsApp%20Video%202025-06-07%20at%2012.18.50_f618cd26.mp4':'Daalwijk 907: invliegende gierzwaluw in spleet onder de dakrand bij de bovenste etage van het flatgebouw. Dit gebeurde tussen 21.45 en 22.30 op een groot aantal verschillende plekken (tussen de 10 en 15 locaties) over de gehele lengte van deze zijde van de flat.'},
                                          "Photos":{'':''}
                                         }
                               },
