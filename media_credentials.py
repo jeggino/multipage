14ef0077873853295a40e87fc9dae34176ab28d5
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-06-07 13:58:46 +0200

    Update media_credentials.py

--- media_credentials.py
+++ media_credentials.py
@@ -1,6 +1,6 @@
 media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube.com/watch?v=OKEaI1mFAhc":" ",
                                                             'https://www.youtube.com/watch?v=6qO1mpw8Xow':'Laatvliegers foerageren boven fietspad te Lies, Terschelling. Lantaarnpaal trekt waarschijnlijk prooien aan en de laatvliegers maken daar gretig gebruik van.',
-                                                            'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp//signal-2025-04-27-105501.mp4':'Franschenland 24, 26-04-2025'},
+                                                            'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp/video-ts/signal-2025-04-27-105501.mp4':'Franschenland 24, 26-04-2025'},
                                                    "Photos":{'<img src="https://www.ecoloogterschelling.nl/wp-content/uploads/2023/09/gewone-dwergvleermuis-invliegend-kolonie.jpeg" width="400" />':'',                                                            
                                                             }
                                                  },
@@ -18,7 +18,7 @@ media_dict = {'SMPs-Terschelling':{'Vleermuizen':{"Videos":{"https://www.youtube
               'SMPs-ZuidOost':{'Vleermuizen':{"Videos":{},
                                               "Photos":{}
                                                          },
-                               'Vogels':{"Videos":{'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp//WhatsApp%20Video%202025-06-07%20at%2012.18.50_f618cd26.mp4':'Daalwijk 907: invliegende gierzwaluw in spleet onder de dakrand bij de bovenste etage van het flatgebouw. Dit gebeurde tussen 21.45 en 22.30 op een groot aantal verschillende plekken (tussen de 10 en 15 locaties) over de gehele lengte van deze zijde van de flat.',
+                               'Vogels':{"Videos":{'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp/video-zo/WhatsApp%20Video%202025-06-07%20at%2012.18.50_f618cd26.mp4':'Daalwijk 907: invliegende gierzwaluw in spleet onder de dakrand bij de bovenste etage van het flatgebouw. Dit gebeurde tussen 21.45 en 22.30 op een groot aantal verschillende plekken (tussen de 10 en 15 locaties) over de gehele lengte van deze zijde van de flat.',
                                                   },
                                          "Photos":{'https://anydhrpvfenefacuoarv.supabase.co/storage/v1/object/public/smp/photos-zo/IMG_6441_edited.jpg':'',
                                                   }
