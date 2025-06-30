85200c95365617e9e0a5bddc8738cd93c7d15c52
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-06-30 20:22:42 +0200

    Update 📝_Dagverlag_formulier.py

--- page/📝_Dagverlag_formulier.py
+++ page/📝_Dagverlag_formulier.py
@@ -16,6 +16,18 @@ from streamlit_option_menu import option_menu
 import random
 
 
+st.markdown(
+    """
+    <style>
+    [data-testid="collapsedControl"] svg {
+        height: 0rem;
+        width: 0rem;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 # st.markdown("""
 #     <style>
 #     .css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; } #MainMenu{ visibility: hidden; } footer { visibility: hidden; } header { visibility: hidden; }
@@ -25,13 +37,13 @@ import random
 
 
 
-# reduce_header_height_style = """
-# <style>
-#     div.block-container {padding-top: 1rem; padding-bottom: 0rem; padding-left: 1rem; padding-right: 1rem; margin-top: 1rem; margin-bottom: 0rem;}
-# </style>
-# """ 
+reduce_header_height_style = """
+<style>
+    div.block-container {padding-top: 1rem; padding-bottom: 0rem; padding-left: 1rem; padding-right: 1rem; margin-top: 1rem; margin-bottom: 0rem;}
+</style>
+""" 
 
-# st.markdown(reduce_header_height_style, unsafe_allow_html=True)
+st.markdown(reduce_header_height_style, unsafe_allow_html=True)
 
 def init_connection():
     url = st.secrets["SUPABASE_URL"]
