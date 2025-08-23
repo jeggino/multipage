8166b393015f2b3df44ff915a56930f90b214715
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-08-23 12:45:32 +0200

    Update 🧭_navigatie.py

--- page/🧭_navigatie.py
+++ page/🧭_navigatie.py
@@ -574,7 +574,7 @@ try:
     map = folium.Map(location=[lat, lng], zoom_start=10,zoom_control=False,tiles=None,font_size= '1rem')
     oude_waarnemingen = folium.FeatureGroup(name="Oude waarnemingen",show=False).add_to(map)
 except:
-    map = folium.Map(tiles=None, zoom_start=8,zoom_control=False,,font_size= '1rem')
+    map = folium.Map(tiles=None, zoom_start=8,zoom_control=False,font_size= '1rem')
 
 kwargs = {'drawCircle':False}
 LocateControl(auto_start=st.session_state.project['auto_start'],position="topleft",**kwargs).add_to(map)
