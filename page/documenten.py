379b270c2df536397e2674769563a83182d8a95b
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-09-05 08:59:46 +0200

    Update documenten.py

--- page/documenten.py
+++ page/documenten.py
@@ -1,1 +1,23 @@
+import streamlit as st
+from streamlit_gsheets import GSheetsConnection
+import pandas as pd
+import geopandas as gpd
+import random
+
+import folium
+from folium.plugins import Draw, Fullscreen, LocateControl, GroupedLayerControl
+from streamlit_folium import st_folium
+import datetime
+from datetime import datetime, timedelta, date
+import random
+
+import ast
+
+from supabase import create_client, Client
+
+from credentials import *
+
+from streamlit_cookies_controller import CookieController
+import time
+
 
