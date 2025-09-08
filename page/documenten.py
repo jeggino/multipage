044d5d86f74eee11f0f3fd4aabb4741d7add709f
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-09-08 09:22:46 +0200

    Update documenten.py

--- page/documenten.py
+++ page/documenten.py
@@ -21,5 +21,5 @@ from streamlit_cookies_controller import CookieController
 import time
 
 
-st.pdf("page/HK_potentiescan_EN.pdf", height="stretch", key=None)
+st.pdf("page/TEST_SMP_APP.pdf", height="stretch", key=None)
 
