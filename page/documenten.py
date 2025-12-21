bb7a98acba11762bfa134c87a746002e6ca61610
Luigi Giugliano <76209116+jeggino@users.noreply.github.com>
2025-12-21 15:41:31 +0100

    Update documenten.py

--- page/documenten.py
+++ page/documenten.py
@@ -20,68 +20,68 @@ from credentials import *
 from streamlit_cookies_controller import CookieController
 import time
 
+st.image('https://img.freepik.com/premium-vector/no-documents-sign-prohibited-paperwork-symbol-disallowed-files-crossed-out-vector_797523-3621.jpg')
+
+# def init_connection():
+#     url = st.secrets["SUPABASE_URL"]
+#     key = st.secrets["SUPABASE_KEY"]
+#     return create_client(url, key)
+
+# supabase = init_connection()
+# rows_points = supabase.table("df_observations").select("*").execute()
+# df_point = pd.DataFrame(rows_points.data)
+
+# df_point = df_point[df_point['geometry_type']=='Point']
+
+# import geopandas as gpd
 
 
-def init_connection():
-    url = st.secrets["SUPABASE_URL"]
-    key = st.secrets["SUPABASE_KEY"]
-    return create_client(url, key)
+# # Create geometry column
+# geometry = gpd.points_from_xy(df_point["lng"], df_point["lat"])
 
-supabase = init_connection()
-rows_points = supabase.table("df_observations").select("*").execute()
-df_point = pd.DataFrame(rows_points.data)
+# # Convert to GeoDataFrame
+# gdf = gpd.GeoDataFrame(df_point, geometry=geometry, crs="EPSG:4326")
+# m = gdf.explore('sp')
 
-df_point = df_point[df_point['geometry_type']=='Point']
-
-import geopandas as gpd
+# map = folium.Map(zoom_start=10,zoom_control=False,font_size= '0.8rem')
+# output = st_folium(map)
 
 
-# Create geometry column
-geometry = gpd.points_from_xy(df_point["lng"], df_point["lat"])
-
-# Convert to GeoDataFrame
-gdf = gpd.GeoDataFrame(df_point, geometry=geometry, crs="EPSG:4326")
-m = gdf.explore('sp')
-
-map = folium.Map(zoom_start=10,zoom_control=False,font_size= '0.8rem')
-output = st_folium(map)
+# from streamlit.components.v1 import html
 
 
-from streamlit.components.v1 import html
+# # Save map to HTML file
+# html_file = "map.html"
+# m.save(html_file)
 
 
-# Save map to HTML file
-html_file = "map.html"
-m.save(html_file)
-
-
-# Option 2: Provide download button
-with open(html_file, "rb") as f:
-    st.download_button(
-        label="Download map as HTML",
-        data=f,
-        file_name="map.html",
-        mime="text/html"
-    )
+# # Option 2: Provide download button
+# with open(html_file, "rb") as f:
+#     st.download_button(
+#         label="Download map as HTML",
+#         data=f,
+#         file_name="map.html",
+#         mime="text/html"
+#     )
 
 
 
 
-# @st.dialog(" ",width='large')
-# def pdf(file):
-#   st.pdf(file, height="stretch", key=None)
+# # @st.dialog(" ",width='large')
+# # def pdf(file):
+# #   st.pdf(file, height="stretch", key=None)
   
-#   with open(file, "rb") as pdf_file:
-#     PDFbyte = pdf_file.read()
+# #   with open(file, "rb") as pdf_file:
+# #     PDFbyte = pdf_file.read()
 
-#   st.download_button(
-#     label="Download pdf",
-#     data=PDFbyte,
-#     file_name="file.pdf",
-#     # mime='application/octet-stream',
-#     icon=":material/download:"
-#   )
+# #   st.download_button(
+# #     label="Download pdf",
+# #     data=PDFbyte,
+# #     file_name="file.pdf",
+# #     # mime='application/octet-stream',
+# #     icon=":material/download:"
+# #   )
 
 
-# if st.button("Rapport"):
-#   pdf("page/TEST_SMP_APP.pdf")
+# # if st.button("Rapport"):
+# #   pdf("page/TEST_SMP_APP.pdf")
